b26cc2be27f497c3cdc06942f20db97ae95b1079
nini kiria <kirianini78@gmail.com>
2022-08-21 13:33:51 +0400

    update app.js

--- src/App.js
+++ src/App.js
@@ -1,3 +1,9 @@
+import {
+  Routes,
+  Route,
+
+} from "react-router-dom";
+
 import BuySection from "./page/BuySection";
 import Header from "./page/Header";
 import MainTech from "./page/MainTech";
@@ -5,9 +11,14 @@ import MainTech from "./page/MainTech";
 function App() {
   return (
     <>
-      <Header />
-      <MainTech />
-      <BuySection />
+         <Routes>
+          <Route path="/" element={ <Header />}></Route>
+          <Route path="/Tech" element={ <MainTech />}></Route>
+          <Route path="/Card" element={  <BuySection />}></Route>
+        </Routes>
+  
+
+
     </>
   );
 }
